a6995c7a147ea39ec867c18cd04bac0315f11628
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-07-26 07:32:52 +0300

    Update sleepy.sh

--- Section 2: Shell Scripting in a Nutshell/sleepy.sh
+++ Section 2: Shell Scripting in a Nutshell/sleepy.sh
@@ -6,3 +6,9 @@ $ ./sleepy.sh &
 $ ps -fp 16796
 UID PID PPID C STIME TTY       TIME CMD
 jason 16796 16725 0 22:50 pts/0 00:00:00
+
+$ /tmp./sleepy.sh &
+[1] 16804
+$ ps -fp 16804
+UID PID PPID C STIME TTY       TIME CMD
+jason 16804 16725 0 22:51 pts/0 00:00:00
