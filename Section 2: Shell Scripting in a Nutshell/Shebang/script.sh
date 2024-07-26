b37f34d14d9075b513f2118e9400687602f362e6
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-07-26 07:28:58 +0300

    Create script.sh

---
+++ Section 2: Shell Scripting in a Nutshell/Shebang/script.sh
@@ -0,0 +1,8 @@
+#!/bin/csh
+echo "This script uses csh as the interpreter."
+
+#!/bin/ksh
+echo "This script uses ksh as the interpreter."
+
+#!/bin/zsh
+echo "This script uses zsh as the interpreter."
