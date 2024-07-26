af676e45ceeaf6b6b6cf856d7192767161b65dc6
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-07-26 12:59:01 +0300

    Update script.sh

--- Section 4: Shell Functions/script.sh
+++ Section 4: Shell Functions/script.sh
@@ -13,3 +13,18 @@ function now() {
     echo "It's $(date +%r)"
 }
 hello
+
+#Positional Parameters
+#Functions can accept parameters.
+#The first parameter is stored in $1.
+#The second parameter is stored in $2, etc.
+#$@ contains all of the parameters.
+#Just like shell scripts $0 = the script itself, not function name
+
+#!/bin/bash
+function hello() {
+    echo "Hello $1"
+}
+hello Jason
+#Output is:
+#Hello Jason
