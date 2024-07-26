019baa83ca9f0c8a8015853cf79d6dee448b6aff
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-07-26 08:29:20 +0300

    Update script.sh

--- Section 2: Shell Scripting in a Nutshell/Shell Scripting, Part 2/script.sh
+++ Section 2: Shell Scripting in a Nutshell/Shell Scripting, Part 2/script.sh
@@ -8,3 +8,67 @@ fi
 
 #Output:
 #You seem to like the bash shell.
+
+#!/bin/bash
+MY_SHELL="csh"
+
+if [ "$MY_SHELL" = "bash" ]
+then
+   echo "You seem to like the bash shell."
+else
+   echo "You don't seem to like the bash shell."
+fi
+
+#!/bin/bash
+MY_SHELL="csh"
+
+if [ "$MY_SHELL" = "bash" ]
+then
+   echo "You seem to like the bash shell."
+elif [ "$MY_SHELL" = "csh" ]
+then
+   echo "You seem to like the csh shell."
+else
+   echo "You don't seem to like the bash or csh shells."
+fi
+
+#!/bin/bash
+for COLOR in red green blue
+do
+  echo "COLOR: $COLOR"
+done
+
+#Output:
+#Color:red
+#Color:green
+#Color:blue
+
+#!/bin/bash
+for COLOR in red green blue
+do 
+  echo "COLOR: $COLOR"
+done
+#Output:
+#Color:red
+#Color:green
+#Color:blue
+
+
+#!/bin/bash
+COLORS="red green blue"
+
+for COLOR in $COLORS
+do 
+  echo "COLOR: $COLOR"
+done
+
+#!/bin/bash
+PICTURES=$(ls *jpg)
+DATE=$(date +%F)
+
+for PICTURE in $PICTURES
+do 
+  echo "Renaming ${PICTURE} to ${DATE}
+-${PICTURE}"
+  mv ${PICTURE} ${DATE}-${PICTURE}
+done
